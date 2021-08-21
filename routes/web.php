c65ded06f1f6737d715de47a5693ad9d7615d8c9
yogitko17 <ykkkaj3172231106yhh_0307@yahoo.co.jp>
2021-08-21 21:43:05 +0900

    kadai

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/building',[TestController::class, 'index']);
+Route::get('/building/{room?}', function ($room) {
+    return "部屋番号は". $room . "です";
+});
